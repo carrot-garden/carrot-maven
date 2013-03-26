b697b2321913634a62cf87ec169a5eaff6c78137
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2013-03-26 13:17:02 -0500

    barchart use case

---
+++ carrot-maven-scr-plugin/carrot-maven-scr-plugin-test-1/src/main/java/HealthReport.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2011-2013 Barchart, Inc. <http://www.barchart.com/>
+ *
+ * All rights reserved. Licensed under the OSI BSD License.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+
+
+public interface HealthReport {
+
+	/**
+	 * Health report URL.
+	 * <p>
+	 * Used in load balancer cluster configuration.
+	 */
+	String PATH = "/health-report";
+
+}
---
+++ carrot-maven-scr-plugin/carrot-maven-scr-plugin-test-1/src/main/java/com/barchart/karaf/test/api/HealthReport.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2011-2013 Barchart, Inc. <http://www.barchart.com/>
+ *
+ * All rights reserved. Licensed under the OSI BSD License.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+package com.barchart.karaf.test.api;
+
+public interface HealthReport {
+
+	/**
+	 * Health report URL.
+	 * <p>
+	 * Used in load balancer cluster configuration.
+	 */
+	String PATH = "/health-report";
+
+}
